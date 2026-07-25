31b8790077e741c87e1d4c4944b2af2fd4210ed8
KaioR <kaioangel.r@gmail.com>
2026-07-25 18:47:40 -0300

    feat: implement ID user router

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,25 @@ const path = require("path");
 
 const usersPath = path.join(__dirname, "..", "data", "users.json");
 
+router.get("/users/:id", (req, res) => {
+  fs.readFile(usersPath, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error reading the users");
+    }
+
+    const users = JSON.parse(data);
+    const user = users.find((item) => item._id === req.params.id);
+
+    if (!user) {
+      res.status(404).send({ message: "User ID not found" });
+      return;
+    }
+
+    res.send(user);
+  });
+});
+
 router.get("/users", (req, res) => {
   fs.readFile(usersPath, { encoding: "utf8" }, (err, data) => {
     if (err) {
